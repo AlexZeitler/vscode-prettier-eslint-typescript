18ec10e5d7e09e64ad82b2a545c4b82db9298839
Christoph Stock <stockulus@icloud.com>
2019-07-07 13:36:22 +0200

    added prettier config in package.json

--- src/GetEnvTyped.ts
+++ src/GetEnvTyped.ts
@@ -1,9 +1,12 @@
-type SimpleType = string | number | boolean;
+type SimpleType = string | number | boolean
 
 interface SimpleConstructor<T extends SimpleType> {
-  (value: any): T;
+  (value: any): T
 }
 
-export function getEnv<T extends SimpleType>(name: string, cast: SimpleConstructor<T>): T { 
-  return cast(process.env[name]);
+export function getEnv<T extends SimpleType>(
+  name: string,
+  cast: SimpleConstructor<T>
+): T {
+  return cast(process.env[name])
 }
